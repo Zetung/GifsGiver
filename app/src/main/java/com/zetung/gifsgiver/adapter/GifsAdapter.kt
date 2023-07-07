dc7b3adb1d0f1ea8d39c5126a8d96e1fda45a2cb
Zetung <v.s.kasperovich@gmail.com>
2023-07-07 16:09:20 +0300

    Add heart view

--- app/src/main/java/com/zetung/gifsgiver/adapter/GifsAdapter.kt
+++ app/src/main/java/com/zetung/gifsgiver/adapter/GifsAdapter.kt
@@ -3,6 +3,7 @@ package com.zetung.gifsgiver.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -13,8 +14,11 @@ import com.zetung.gifsgiver.model.DataObject
 
 
 class GifsAdapter(val context: Context,var gifs: List<DataObject>) : RecyclerView.Adapter<GifsAdapter.ViewHolder>(){
+
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView = itemView.findViewById<ImageView>(R.id.ivGif)
+        val likeButton = itemView.findViewById<View>(R.id.likeButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,10 +32,15 @@ class GifsAdapter(val context: Context,var gifs: List<DataObject>) : RecyclerVie
         return gifs.size
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = gifs[position]
 
         Glide.with(context).load(data.images.gif.url).into(holder.imageView)
+
+        holder.likeButton.setOnClickListener {
+            it.setBackgroundColor(R.color.purple)
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
