6eb8c04c84bf821cecddbee15025e0f5ea52d476
Zetung <v.s.kasperovich@gmail.com>
2023-07-07 15:00:52 +0300

    Add request log

--- app/src/main/java/com/zetung/gifsgiver/MainActivity.kt
+++ app/src/main/java/com/zetung/gifsgiver/MainActivity.kt
@@ -80,8 +80,9 @@ class MainActivity : AppCompatActivity() {
     class LoggingInterceptor : Interceptor {
         override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
             val request = chain.request()
+            Log.d("Retrofit request",request.toString())
             val response = chain.proceed(request)
-            Log.d("Retrofit", response.toString())
+            Log.d("Retrofit response", response.toString())
             return response
         }
     }
