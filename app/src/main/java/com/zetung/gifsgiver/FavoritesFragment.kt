90215e9ee0c0965acf55d500ecd90baaba58f6bb
Zetung <v.s.kasperovich@gmail.com>
2023-07-10 16:33:42 +0300

    Add view for favorite fragment

--- app/src/main/java/com/zetung/gifsgiver/FavoritesFragment.kt
+++ app/src/main/java/com/zetung/gifsgiver/FavoritesFragment.kt
@@ -1,23 +1,49 @@
 package com.zetung.gifsgiver
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zetung.gifsgiver.adapter.GifsAdapter
+import com.zetung.gifsgiver.databinding.FragmentFavoritesBinding
+import com.zetung.gifsgiver.model.DataGif
+import com.zetung.gifsgiver.model.DataObject
+import com.zetung.gifsgiver.model.Gif
 
 class FavoritesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var con: Context
+
+    private lateinit var adapter: GifsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        con = requireContext()
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    override fun onResume() {
+        super.onResume()
+
+        val gifs = mutableListOf<DataObject>()
+        val source1 = DataObject(DataGif(Gif("https://media4.giphy.com/media/1o16YVH3PTUFQ8uskW/giphy.gif?cid=a4fb3c122btpk3zbnotr4z6hanipkvmhr8u1kvqnfcnh6jil&ep=v1_gifs_trending&rid=giphy.gif&ct=g")))
+        gifs.add(source1)
+        gifs.add(source1)
+        gifs.add(source1)
+
+        adapter = GifsAdapter(con,gifs)
+        binding.gifView.layoutManager = LinearLayoutManager(con)
+        binding.gifView.adapter = adapter
+
     }
 
 
