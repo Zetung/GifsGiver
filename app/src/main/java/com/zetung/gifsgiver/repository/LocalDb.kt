6038559c8b4476812da812d0e02dd797f011b8d1
Zetung <v.s.kasperovich@gmail.com>
2023-07-19 16:04:07 +0300

    Add migration for Room

--- app/src/main/java/com/zetung/gifsgiver/repository/LocalDb.kt
+++ app/src/main/java/com/zetung/gifsgiver/repository/LocalDb.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.zetung.gifsgiver.repository.model.GifModel
 
-@Database(entities = [GifModel::class], version = 1)
+@Database(entities = [GifModel::class], version = 2)
 abstract class LocalDb: RoomDatabase() {
     abstract fun getFavoritesDAO(): GifDAO
     companion object{
@@ -15,7 +17,17 @@ abstract class LocalDb: RoomDatabase() {
                 context.applicationContext,
                 LocalDb::class.java,
                 "GifsGiver.db"
-            ).build()
+            ).addMigrations(MIGRATION_1_2).build()
         }
     }
+}
+
+val MIGRATION_1_2 = object : Migration(1,2){
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("CREATE TABLE IF NOT EXISTS `gifs` " +
+                "(`id` TEXT NOT NULL, `url` TEXT NOT NULL, `like` INTEGER NOT NULL, PRIMARY KEY(`id`))")
+        database.execSQL("INSERT INTO `gifs` (`id`, `url`, `like`) SELECT `id`, `url`, 1 FROM `favorites`")
+        database.execSQL("DROP TABLE IF EXISTS `favorites`")
+    }
+
 }
