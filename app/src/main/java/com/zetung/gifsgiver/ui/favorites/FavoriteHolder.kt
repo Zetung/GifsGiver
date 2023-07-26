f2727ba060733386332fa8aeb6ee97533342ee1b
Zetung <v.s.kasperovich@gmail.com>
2023-07-26 15:52:37 +0300

    Add error view for gifs

--- app/src/main/java/com/zetung/gifsgiver/ui/favorites/FavoriteHolder.kt
+++ app/src/main/java/com/zetung/gifsgiver/ui/favorites/FavoriteHolder.kt
@@ -1,10 +1,15 @@
 package com.zetung.gifsgiver.ui.favorites
 
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.zetung.gifsgiver.R
 import com.zetung.gifsgiver.repository.model.GifModel
 import com.zetung.gifsgiver.ui.OnLikeClickListener
@@ -14,7 +19,9 @@ class FavoriteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val likeButton = itemView.findViewById<CheckBox>(R.id.likeButton)
 
     fun bind(data: GifModel, listener: OnLikeClickListener){
-        Glide.with(itemView.context).load(data.url).into(imageView)
+        Glide.with(itemView.context)
+            .load(data.url).error(R.drawable.error24)
+            .into(imageView)
         likeButton.isChecked = data.like
         likeButton.setOnClickListener {
             listener.onLikeClick(adapterPosition, data)
